ed5cd8f18f8b792dceb13c4e580f08914759fa21
sseltref <szymonmilosz99@gmail.com>
2022-04-06 21:57:22 +0200

    Revert "added png chart exporting function and kivy chart importing"
    This reverts commit 2d816e0a

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ kivy.require('2.0.0')
 
 from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-import yfinance as yf
-import pandas as pd
-import matplotlib.pyplot as plt
-from kivy.uix.image import Image
+#from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
-class Button(Button):
+class AnswerInput(BoxLayout):
     pass
 
 class Main(TabbedPanel):
@@ -18,29 +14,14 @@ class Main(TabbedPanel):
 
 
 class MyApp(App):
-    img_src = 'foo.png'
-
-    def stock_data(self, ticker, period, interval, observation):
-        ticker = yf.Ticker(ticker)
-        ticker_history = ticker.history(period, interval)
-
-        sf = ticker_history[observation]
-        df = pd.DataFrame({'Date': sf.index, 'Values': sf.values})
-
-        x = df['Date'].tolist()
-        y = df['Values'].tolist()
-
-        plt.style.use('dark_background')
-        plt.plot(x, y)
-        plt.ylabel('Price($)')
-        plt.xlabel('Date', rotation=0)
-        plt.savefig('foo.png')
 
     def build(self):
         #box = BoxLayout()
         #box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
         #return box
         return  Main()
+
+
 if __name__ == '__main__':
     MyApp().run()
 
