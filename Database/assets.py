090d05590a93c6f536c6fe73b9db906099da6075
krzysztofbober <52881815+krzysztofbober@users.noreply.github.com>
2022-05-17 23:05:03 +0200

    Add files via upload
    new function added, that is calculating value of every asset in portfolio

--- Database/assets.py
+++ Database/assets.py
@@ -1,5 +1,7 @@
 import sqlite3
 from example_query import create_connection
+import yfinance as yf
+
 
 def assets_to_sell():
 	export = []
@@ -13,4 +15,34 @@ def assets_to_sell():
 
 	return export
 
+def value(ticker=None):
+	values = {}
+	conn = create_connection('database.db')
+	cur = conn.cursor()
 
+
+	cur.execute("SELECT * FROM history")
+
+	for line in cur.fetchall():
+		c1, c2, v = line[2], line[3], float(line[4])
+
+		if c1 not in values:
+			values[c1] = 0
+		else:
+			values[c1] += v
+
+		if c2 not in values:
+			values[c2] = 0
+		else:
+			values[c2] -= v
+
+		for line in values:
+			values[line] = round(values[line], 2)
+
+	if ticker == None:
+		return values
+	else:
+		return values[ticker]
+
+
+print(value('CHF'))
